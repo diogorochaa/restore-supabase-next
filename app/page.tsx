2956cf64799c8b2f129c71b74206b1fffa80b502
Diogo Rocha <diogo.rocha@agi.com.br>
2023-09-28 18:44:18 -0300

    feat: create account and login, supabase methods

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,50 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
-import { RedirectType, redirect } from "next/navigation"
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { RedirectType, redirect } from 'next/navigation'
 
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "../components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs'
+import CreateAccountForm from '../components/auth/create-account-form'
+import LoginAccountForm from '../components/auth/loggin-account-form'
 
 export default async function Home() {
   let loggedIn = false
   try {
     const supabase = createServerComponentClient({ cookies })
-    const { data: {session},} = await supabase.auth.getSession()
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
 
     if (session) {
       loggedIn = true
     }
-
   } catch (e) {
     console.log('home', e)
-
   } finally {
     if (loggedIn) redirect('/user-app', RedirectType.replace)
   }
 
   return (
     <div className="flex flex-col h-screen w-full justify-center items-center">
-      <Tabs defaultValue="create-account" className="w-[400px] border rounded-md pb-4 shadow-2xl">
+      <Tabs
+        defaultValue="create-account"
+        className="w-[400px] border rounded-md pb-4 shadow-2xl"
+      >
         <TabsList className="flex justify-around items-center rounded-b-none h-14">
-          <TabsTrigger className="transition-all delay-150" value="create-account">Account</TabsTrigger>
-          <TabsTrigger className="transition-all delay-150" value="login">Login</TabsTrigger>
+          <TabsTrigger
+            className="transition-all delay-150"
+            value="create-account"
+          >
+            Account
+          </TabsTrigger>
+          <TabsTrigger className="transition-all delay-150" value="login">
+            Login
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="create-account">
-         
+          <CreateAccountForm />
         </TabsContent>
         <TabsContent value="login">
-         
+          <LoginAccountForm />
         </TabsContent>
       </Tabs>
     </div>
---
+++ components/auth/loggin-account-form.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+
+// definindo schema de validação
+const formSchema = z.object({
+  email: z
+    .string({ required_error: 'E-mail is required' })
+    .email({ message: 'Must be a valid e-mail' }),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .min(7, { message: 'Password must be at least 7 characters long' })
+    .max(12, { message: 'Password must be at most 12 characters long' }),
+})
+
+export default function LoginAccountForm() {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const supabase = createClientComponentClient()
+      const { email, password } = values
+      const {
+        error,
+        data: { session },
+      } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
+      if (error) throw error
+
+      form.reset()
+      router.refresh()
+    } catch (error) {
+      console.log('login form: on submit', error)
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center space-y-2">
+      <span>Login Account Form</span>
+      <Form {...form}>
+        <form
+          className="flex flex-col space-y-2"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="email">E-mail</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="E-mail"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>This is your e-mail address</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>This is your password</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Login</Button>
+        </form>
+      </Form>
+    </div>
+  )
+}
